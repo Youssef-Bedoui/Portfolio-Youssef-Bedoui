b6d3779e17903872dbe3da6d713d52383ba36b62
Youssef Bedoui <Jozefbedoui@gmail.com>
2023-10-16 12:26:21 +0100

    fix deployment bug

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
+      <Routes basename="/Portfolio-Youssef-Bedoui">
         <Route exact path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
