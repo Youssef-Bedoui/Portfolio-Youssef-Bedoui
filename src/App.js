50615e3b4f9031c69730a6c820e987d54c88822a
Youssef Bedoui <Jozefbedoui@gmail.com>
2023-10-16 15:10:56 +0100

    fix style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.scss";
 import About from "./components/layout/About/about";
 import Contact from "./components/layout/Contact/contact";
+import Footer from "./components/layout/Footer/footer";
 import Hero from "./components/layout/Hero/hero";
 import Navbar from "./components/layout/navbar/navbar";
 import Portfolio from "./components/layout/Portfolio/portfolio";
 import Resume from "./components/layout/Resume/resume";
 import Skills from "./components/layout/Skills/skills";
 import KeyboardDoubleArrowUpIcon from "@mui/icons-material/KeyboardDoubleArrowUp";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 function App() {
   const scrollToTop = () => {
@@ -14,6 +17,22 @@ function App() {
   };
   return (
     <div className="App">
+      <div className="rightFloat">
+        <a
+          href="https://www.linkedin.com/in/youssef-bedoui-63661a201/"
+          alt="linkedin"
+          className="socialBtn"
+        >
+          <LinkedInIcon className="icon" />
+        </a>
+        <a
+          href="https://www.facebook.com/profile.php?id=100004288716633"
+          alt="facebook"
+          className="socialBtn"
+        >
+          <FacebookIcon className="icon" />
+        </a>
+      </div>
       <Navbar />
       <Hero />
       <About />
@@ -21,6 +40,7 @@ function App() {
       <Resume />
       <Portfolio />
       <Contact />
+      <Footer />
       <div className="scrollToTop" onClick={() => scrollToTop()}>
         <KeyboardDoubleArrowUpIcon className="scroll_icon" />
       </div>
---
+++ src/components/layout/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.scss";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Coded and Designed By Youssef Bedoui | Web Developer &copy; 2023</p>
+    </footer>
+  );
+}
+
+export default Footer;
