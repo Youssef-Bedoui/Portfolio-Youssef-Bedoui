7a695857a5d1cc5d6fa48b6601d7ff1cb3cc8fe5
Youssef Bedoui <Jozefbedoui@gmail.com>
2023-10-16 12:58:21 +0100

    fix deployment bug

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes basename={"/Portfolio-Youssef-Bedoui"}>
+    <Router basename={"/Portfolio-Youssef-Bedoui"}>
+      <Routes>
         <Route exact path="/" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
