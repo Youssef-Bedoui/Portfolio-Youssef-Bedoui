7708894d3e97a5410669464b8e1c2b735ad5f7e8
Youssef Bedoui <Jozefbedoui@gmail.com>
2023-11-07 22:46:50 +0100

    add new portfolio project

--- src/components/layout/Portfolio/portfolio.js
+++ src/components/layout/Portfolio/portfolio.js
@@ -4,6 +4,7 @@ import Klupea_img from "../../../assets/images/portfolio/klupea/Klupea_main.png"
 import EvenToGo_img from "../../../assets/images/portfolio/Eventogo/EvenToGo - Opera 28_04_2023 12_40_49 AM.png";
 import MovieLights_img from "../../../assets/images/portfolio/MovieLights/React App - Opera 14_05_2023 9_09_16 PM.png";
 import RainyForecast_img from "../../../assets/images/portfolio/RainyForecast/React App - Opera 18_10_2023 8_25_17 PM.png";
+import Al_Aajel_img from "../../../assets/images/portfolio/Al_Aajel_News/Al Aajel News et 6 pages de plus - Personnel – Microsoft​ Edge 07_11_2023 9_58_48 PM.png";
 import ProjectDetailModal from "./projectDetail/projectDetail";
 
 function Portfolio() {
@@ -73,11 +74,23 @@ function Portfolio() {
           >
             <img
               src={RainyForecast_img}
-              alt="Klupea"
+              alt="Rainy Forecast"
               className="project_image"
             />
             <div className="project_details">
               <div className="project_name">Rainy Forecast</div>
+              <div className="project_type">Website</div>
+            </div>
+          </div>
+          <div
+            className="project_item"
+            onClick={() => {
+              openModal(4);
+            }}
+          >
+            <img src={Al_Aajel_img} alt="Al Aajel" className="project_image" />
+            <div className="project_details">
+              <div className="project_name">Al Aajel News</div>
               <div className="project_type">Website</div>
             </div>
           </div>
